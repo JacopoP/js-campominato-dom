4ee003c68dda1786d44050ccc0d0246373ea2881
Jac22 <jacopo.prudentino@gmail.com>
2022-11-17 15:56:22 +0100

    Aggiunta scelta di numero righe e colonne

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -7,12 +7,14 @@ document.getElementById("generate").addEventListener("click",
         container.classList.remove("hide");
         container.innerHTML="";
         console.clear();
-        const n=parseInt(document.getElementById("difficulty").value);
+        const x=parseInt(document.getElementById("col").value);
+        const y=parseInt(document.getElementById("row").value);
+        container.style.aspectRatio=x/y;
         const mines=parseInt(document.getElementById("mines").value);
-        const bombArr=ArrRandNumGen(mines, 1, n*n);
+        const bombArr=ArrRandNumGen(mines, 1, x*y);
         let score=0;
-        for(let i=0; i<n*n; i++){
-            const newElement = createBox(n);            
+        for(let i=0; i<x*y; i++){
+            const newElement = createBox(x, y);            
             if(bombArr.includes(i+1)){
                 newElement.classList.add("bomb");
             }
@@ -23,7 +25,7 @@ document.getElementById("generate").addEventListener("click",
                     if(!this.classList.contains("selected")){
                         this.classList.add("selected");
                         if(this.classList.contains("bomb")){                            
-                            for(let k=0; k<n*n; k++){
+                            for(let k=0; k<x*y; k++){
                                 table[k].classList.add("selected")
                             }
                             result.classList.remove("hide");
@@ -35,32 +37,32 @@ document.getElementById("generate").addEventListener("click",
                             score++;
                             let m=0
 
-                            if(((i+1)<=n && i!=0 && i!=(n-1))){
+                            if(((i+1)<=x && i!=0 && i!=(x-1))){
                                 if(table[i-1].classList.contains("bomb")){
                                     m++;
                                 }
                                 if(table[i+1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n-1].classList.contains("bomb")){
+                                if(table[i+x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n].classList.contains("bomb")){
+                                if(table[i+x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n+1].classList.contains("bomb")){
+                                if(table[i+x+1].classList.contains("bomb")){
                                     m++;
                                 }
                             }
             
-                            else if((i+1)>(n*(n-1)) && i%n!=0 && (i+1)!=(n*n)){
-                                if(table[i-n-1].classList.contains("bomb")){
+                            else if((i+1)>(x*(y-1)) && i%x!=0 && (i+1)!=(x*y)){
+                                if(table[i-x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n].classList.contains("bomb")){
+                                if(table[i-x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n+1].classList.contains("bomb")){
+                                if(table[i-x+1].classList.contains("bomb")){
                                     m++;
                                 }
                                 if(table[i-1].classList.contains("bomb")){
@@ -75,31 +77,31 @@ document.getElementById("generate").addEventListener("click",
                                 if(table[i+1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n].classList.contains("bomb")){
+                                if(table[i+x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n+1].classList.contains("bomb")){
+                                if(table[i+x+1].classList.contains("bomb")){
                                     m++;
                                 }
                             }
             
-                            else if((i+1)==n){
+                            else if((i+1)==x){
                                 if(table[i-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n-1].classList.contains("bomb")){
+                                if(table[i+x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n].classList.contains("bomb")){
+                                if(table[i+x].classList.contains("bomb")){
                                     m++;
                                 }
                             }
             
-                            else if((i+1)==(n*(n-1)+1)){
-                                if(table[i-n].classList.contains("bomb")){
+                            else if((i)==(x*(y-1))){
+                                if(table[i-x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n+1].classList.contains("bomb")){
+                                if(table[i-x+1].classList.contains("bomb")){
                                     m++;
                                 }
                                 if(table[i+1].classList.contains("bomb")){
@@ -107,11 +109,11 @@ document.getElementById("generate").addEventListener("click",
                                 }
                             }
             
-                            else if((i+1)==(n*n)){
-                                if(table[i-n-1].classList.contains("bomb")){
+                            else if((i+1)==(x*y)){
+                                if(table[i-x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n].classList.contains("bomb")){
+                                if(table[i-x].classList.contains("bomb")){
                                     m++;
                                 }
                                 if(table[i-1].classList.contains("bomb")){
@@ -119,50 +121,50 @@ document.getElementById("generate").addEventListener("click",
                                 }
                             }
             
-                            else if(i%n==0){
-                                if(table[i-n].classList.contains("bomb")){
+                            else if(i%x==0){
+                                if(table[i-x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n+1].classList.contains("bomb")){
+                                if(table[i-x+1].classList.contains("bomb")){
                                     m++;
                                 }
                                 if(table[i+1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n].classList.contains("bomb")){
+                                if(table[i+x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n+1].classList.contains("bomb")){
+                                if(table[i+x+1].classList.contains("bomb")){
                                     m++;
                                 }
                             }
             
-                            else if((i+1)%n==0){
-                                if(table[i-n-1].classList.contains("bomb")){
+                            else if((i+1)%x==0){
+                                if(table[i-x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n].classList.contains("bomb")){
+                                if(table[i-x].classList.contains("bomb")){
                                     m++;
                                 }
                                 if(table[i-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n-1].classList.contains("bomb")){
+                                if(table[i+x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n].classList.contains("bomb")){
+                                if(table[i+x].classList.contains("bomb")){
                                     m++;
                                 }
                             }
             
                             else{
-                                if(table[i-n-1].classList.contains("bomb")){
+                                if(table[i-x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n].classList.contains("bomb")){
+                                if(table[i-x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i-n+1].classList.contains("bomb")){
+                                if(table[i-x+1].classList.contains("bomb")){
                                     m++;
                                 }
                                 if(table[i-1].classList.contains("bomb")){
@@ -171,13 +173,13 @@ document.getElementById("generate").addEventListener("click",
                                 if(table[i+1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n-1].classList.contains("bomb")){
+                                if(table[i+x-1].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n].classList.contains("bomb")){
+                                if(table[i+x].classList.contains("bomb")){
                                     m++;
                                 }
-                                if(table[i+n+1].classList.contains("bomb")){
+                                if(table[i+x+1].classList.contains("bomb")){
                                     m++;
                                 }
                             }
@@ -189,10 +191,10 @@ document.getElementById("generate").addEventListener("click",
                                 this.innerHTML=m;
                             }
 
-                            if(score==(n*n-mines)){
+                            if(score==(x*y-mines)){
                                 result.classList.remove("hide");
                                 result.innerHTML=`Hai vinto!`
-                                for(let k=0; k<n*n; k++){
+                                for(let k=0; k<x*y; k++){
                                     table[k].classList.add("selected")
                                 }
                             }
@@ -214,11 +216,11 @@ document.getElementById("generate").addEventListener("click",
     }
 );
 
-function createBox(row){
+function createBox(row, col){
     const element=document.createElement("div");
     element.classList="box";
     element.style.width = 100/row + "%";
-    element.style.height = 100/row + "%";
+    element.style.height = 100/col + "%";
     return element;
 }
 
